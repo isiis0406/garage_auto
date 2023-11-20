580e35869e79dd61b86e71262114b0de8ff4db8d
isiis006 <issabobobaldepro@gmail.com>
2023-11-21 00:38:25 +0100

    Smtp secure to false

--- client/src/Pages/admin/testimonials/AddTestimonials.js
+++ client/src/Pages/admin/testimonials/AddTestimonials.js
@@ -18,7 +18,6 @@ const initialState = {
     rating: ''
 }
 function AddTestimonials() {
-    useRedirectLoggedUser('/login');
 
     const isLoggedIn = useSelector(selectIsLoggedInd);
     const dispatch = useDispatch();
